819a42ba4c57051183465da843b71495d8b9ccb3
Orzo Cogorzo <orzocogorzo@hotmail.com>
2023-02-20 00:03:43 +0100

    feat: copy-id on install script

--- sh/install.sh
+++ sh/install.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 DIR=/opt/dynipt-client
+read -p "Remote host IP/FQDN: " DYNIPT_HOST
 
 # System requirements
 sudo apt update
@@ -17,6 +18,7 @@ sudo chown -R dynipt: $DIR
 sudo -u dynipt mkdir $DIR/.ssh
 sudo chmod 700 $DIR/.ssh
 sudo -u dynipt ssh-keygen -a 100 -t rsa -N "" -C "sshuttle_key" -f $DIR/.ssh/id_rsa
+ssh-copy-id -i $DIR/.ssh/id_rsa.pub dynipt@$DYNIPT_HOST
 
 # SystemD configuration
 sudo cp $DIR/snippets/systemd.service /etc/systemd/system/dynipt-client.service
@@ -28,7 +30,6 @@ sudo -u dynipt python3 -m venv .venv
 sudo -u dynipt .venv/bin/python -m pip install -r requirements.txt
 
 # DynIPt config
-read -p "Remote host IP/FQDN: " DYNIPT_HOST
 cat <<EOF > $DIR/config.json
 {
     "$DYNIPT_HOST": ["0.0.0.0/0"]
@@ -36,7 +37,7 @@ cat <<EOF > $DIR/config.json
 EOF
 
 # Client start
-sh/service.sh start
+$DIR/.venv/bin/python $DIR/main.py start
 
 echo "DynIPt client is running"
 echo "Your public IP is: $(curl ip.yunohost.org)"
