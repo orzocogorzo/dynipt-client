e8c18b4cf177eb509e758f97998a2b5e9c338a01
Orzo Cogorzo <orzocogorzo@hotmail.com>
2023-02-21 17:05:58 +0100

    feat: switch to .env file for network config

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 import os
-import sys
 import re
-import json
-import signal
-import time
 import socket
 import subprocess
-from subprocess import CalledProcessError
 import logging
 import logging.handlers
 
@@ -26,59 +21,50 @@ conf = f"{home}/config.json"
 ssh_user = os.getenv("DYNIPT_USR", "dynipt")
 
 def run():
-    with open(conf) as jsondata:
-        data = json.load(jsondata)
-    
-    for rhost in data.keys():
-        xhost = socket.gethostbyname(rhost.split(":")[0])
-        netrange = ""
+    rhost = os.getenv("DYNIPT_HOST", "")
+    xhost = socket.gethostbyname(rhost.split(":")[0])
+    networks = os.getenv("DYNIPT_NETWORKS", "0.0.0.0/0")
+    netrange = ""
 
-        # if single network, turn into List
-        if not type(data[rhost]) is list:
-            networks = data[rhost].split()
+    for network in networks.split(","):
+
+        if "/" in network:
+            netrange = netrange + " " + network
         else:
-            networks = data[rhost]
+            netrange = netrange + " " + socket.gethostbyname(network)
 
-        for network in networks:
-            
-            # check if CIDR format
-            if "/" in network:
-                netrange = netrange + " " + network
-            else:
-                netrange = netrange + " " + socket.gethostbyname(network)
-        netrange = netrange.strip()
-        
-        try:
-            log.info("starting dynipt client for networks: %s via %s" % (netrange, rhost))
-            p = subprocess.Popen(" ".join([
-                    "echo",
-                    os.getenv("DYNIPT_PWD", ""),
-                    "|",
-                    "sudo",
-                    "--stdin",
-                    binary_path,
-                    "-r",
-                    f"{ssh_user}@{rhost}",
-                    "-x",
-                    xhost,
-                    *netrange.split(" "),
-                    "--ssh-cmd",
-                    f"'ssh -o StrictHostKeyChecking=no -i {home}/.ssh/id_rsa -o ServerAliveInterval=60'",
-                    "--no-latency-control"
-                ]),
-                stdin=subprocess.PIPE,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-                shell=True,
-            )
+    netrange = netrange.strip()
+    try:
+        log.info("starting dynipt client for networks: %s via %s" % (netrange, rhost))
+        p = subprocess.Popen(" ".join([
+                "echo",
+                os.getenv("DYNIPT_PWD", ""),
+                "|",
+                "sudo",
+                "--stdin",
+                binary_path,
+                "-r",
+                f"{ssh_user}@{rhost}",
+                "-x",
+                xhost,
+                *netrange.split(" "),
+                "--ssh-cmd",
+                f"'ssh -o StrictHostKeyChecking=no -i {home}/.ssh/id_rsa -o ServerAliveInterval=60'",
+                "--no-latency-control"
+            ]),
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            shell=True,
+        )
 
-            out, err = p.communicate(input=os.getenv("DYNIPT_PWD", "").encode())
-            if err and re.search(r"\[sudo\] password for", err.decode()):
-                raise Exception(err.decode())
+        out, err = p.communicate(input=os.getenv("DYNIPT_PWD", "").encode())
+        if err and re.search(r"\[sudo\] password for", err.decode()):
+            raise Exception(err.decode())
 
-        except Exception as err:
-            err = "Error running dynipt client: %s" % str(err)
-            log.error(err)
+    except Exception as err:
+        err = "Error running dynipt client: %s" % str(err)
+        log.error(err)
 
 if __name__ == "__main__":
     run()
