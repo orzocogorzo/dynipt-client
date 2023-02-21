540020d95604b14b0e3432e0c53f344e21b2d7c2
Orzo Cogorzo <orzocogorzo@hotmail.com>
2023-02-21 12:27:23 +0100

    feat: quite installation

--- sh/install.sh
+++ sh/install.sh
@@ -1,43 +1,70 @@
 #! /bin/bash
 
+echo "Start dynipt-client installation"
+
 DIR=/opt/dynipt-client
 read -p "Remote host IP/FQDN: " DYNIPT_HOST
-read -p "DynIPt user password: " DYNIPT_PWD
+read -p "DynIPt remote host user: " DYNIPT_USR
+# read -p "DynIPt user password: " DYNIPT_PWD
+echo
 
 # System requirements
-sudo apt update
-sudo apt install -y python3 python3-venv git iptables
-
-# User creation
-sudo useradd -d $DIR -s /usr/sbin/nologin dynipt
-sudo usermod -aG sudo dynipt
+echo "Install system requirements..."
+sudo apt -qq update > /dev/null 2>&1
+sudo apt install -qqy python3 python3-venv git iptables curl > /dev/null 2>&1
+echo "System requirements installeds"
+echo
 
 # Package download
-sudo git clone https://github.com/orzocogorzo/dynipt-client.git $DIR
-sudo sh -c "echo "DYNIPT_PWD=$DYNIPT_PWD" > $DIR/.env" && sudo chmod 600 $DIR/.env
-sudo chown -R dynipt: $DIR
+echo "Get source code..."
+sudo git clone -q https://github.com/orzocogorzo/dynipt-client.git $DIR
+echo "Source code placed on $DIR"
+echo
 
-# Identity
-sudo -u dynipt mkdir $DIR/.ssh
-sudo chmod 700 $DIR/.ssh
-sudo -u dynipt ssh-keygen -a 100 -t rsa -N "" -C "sshuttle_key" -f $DIR/.ssh/id_rsa
-read -p "DynIPt remote host user: " DYNIPT_RUSER
-sudo -u dynipt ssh-copy-id -i $DIR/.ssh/id_rsa.pub $DYNIPT_RUSER@$DYNIPT_HOST
+# User creation
+echo "Create system user 'dynipt'..."
+sudo useradd -d $DIR -s /usr/sbin/nologin dynipt > /dev/null
+sudo usermod -aG sudo dynipt > /dev/null
+sudo chown -R dynipt: $DIR > /dev/null
+DYNIPT_PWD=$(date +%s | sha256sum | base64 | head -c 32; echo)
+echo -e "$DYNIPT_PWD\n$DYNIPT_PWD" | sudo passwd dynipt > /dev/null
+sudo -u dynipt sh -c "echo "DYNIPT_PWD=$DYNIPT_PWD" > $DIR/.env"
+sudo -u dynipt sh -c "echo "DYNIPT_USR=$DYNIPT_USR" >> $DIR/.env"
+sudo chmod 600 $DIR/.env
+echo "'dynipt' user created"
+echo
+
+# SSH credentials
+echo "Configuring ssh dynipt credentials..."
+sudo -u dynipt mkdir -p $DIR/.ssh
+sudo -u dynipt chmod 700 $DIR/.ssh
+sudo -u dynipt test ! -f $DIR/.ssh/id_rsa && sudo -u dynipt ssh-keygen -a 100 -t rsa -N "" -C "sshuttle_key" -f $DIR/.ssh/id_rsa > /dev/null
+sudo -u dynipt ssh-copy-id -o StrictHostKeyChecking=no -i $DIR/.ssh/id_rsa.pub $DYNIPT_USR@$DYNIPT_HOST > /dev/null 2>&1
+echo "dynipt ssh credentials createds"
+echo
 
 # SystemD configuration
+echo "Configuring dynipt-client systemd service..."
 sudo cp $DIR/snippets/systemd.service /etc/systemd/system/dynipt-client.service
 sudo systemctl daemon-reload
+echo "SystemD service created"
+echo
 
 # Python requirements
+echo "Install python dependencies..."
 cd $DIR
 sudo -u dynipt python3 -m venv .venv
-sudo -u dynipt .venv/bin/python -m pip install -r requirements.txt
+sudo -u dynipt .venv/bin/python -m pip install -r requirements.txt > /dev/null
+echo "Python is ready"
+echo
 
 # DynIPt config
+echo "Store network configurations"
 sudo -u dynipt sh -c "echo '{\"$DYNIPT_HOST\": [\"0.0.0.0/0\"]}' > $DIR/config.json"
+echo
 
 # Client start
 sudo systemctl start dynipt-client.service
 
 echo "DynIPt client is running"
-echo "Your public IP is: $(curl ip.yunohost.org)"
+echo "Your public IP is: $(curl -s ip.yunohost.org)"
